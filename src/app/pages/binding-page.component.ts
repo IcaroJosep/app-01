fc68fea4040aca4d5dd6f46e87fcfa0026211d97
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-04-13 15:17:22 -0300

    .

---
+++ src/app/pages/binding-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-binding',
+    template:'
+        <h1>imagem</h1> 
+        <img src="https://s2-techtudo.glbimg.com/SSAPhiaAy_zLTOu3Tr3ZKu2H5vg=/0x0:1024x609/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/c/u/15eppqSmeTdHkoAKM0Uw/dall-e-2.jpg"/> 
+    '
+})
+
+
+export class BindingPageComponent{
+
+}
