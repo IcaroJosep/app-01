83f10b56107d179e46140d047fee1749423d266c
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-04-13 22:15:59 -0300

    binding de estilo
    aula de inplementacao de estilo com binding
    obs: nunca mas vou me esquecer a diferen;a de ` e '  hahahah

--- src/app/pages/binding-page.component.ts
+++ src/app/pages/binding-page.component.ts
@@ -5,10 +5,35 @@ import { Component } from "@angular/core";
     template: `
         <h1>Imagem</h1>  
         <img [src]="imagem" [style]="{width: width}"/>  
-    `
+        <hr>
+        <br>
+
+
+        <h1>Utilizando class binding para aplicar estilos</h1>
+        <div class="alert" [class]="{sucesso:valor}">
+            alerta
+        </div>
+        <button (click)="enviardados()"> enviar dados</button>
+        `,
+        styles:[`
+            .alert{
+                width:200px;
+                height: 100px;
+                border: 1px solid blue;
+            }
+
+            .sucesso{
+                border: 5px solid green;
+            }  
+       `]    
 })
 export class BindingPageComponent { 
    
     width ="300px"
     imagem="https://avatars.githubusercontent.com/u/147887309?v=4"
+
+    valor=true;
+    enviardados(){
+        this.valor=!this.valor
+    }
 }
